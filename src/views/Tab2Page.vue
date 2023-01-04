f9846dd7d2a9474900aae1d72f4571a27d2e372c
kulisjurai <jura.kulis@yahoo.com>
2023-01-04 19:27:03 +0100

    ionic init

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -11,13 +11,11 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -22,7 +22,7 @@
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
 </script>
